6ff72c8458d825c8921a13dbfec454fe91638c12
soyebsuvo <soyebbeen@gmail.com>
2023-11-07 16:48:16 +0600

    single food items api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000; 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middlewares
 app.use(cors());
@@ -27,13 +27,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect(); 
+    // await client.connect(); 
     const database = client.db('foodDB');
     const foodsCollection = database.collection('foods');
 
     app.get('/foods' , async ( req , res) => {
         const result = await foodsCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/food/:id' , async (req , res) => {
+        const id = req.params.id;
+        console.log(id)
+        const query = { _id : new ObjectId(id)};
+        const result = await foodsCollection.findOne(query);
+        res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
